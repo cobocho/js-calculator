68c76c31a4d5c0a7ed16686229275cd319788375
cobocho <rlaalsrb1111@naver.com>
2023-07-19 16:19:19 +0900

    Test: 나머지 테스트 케이스 작성

--- __tests__/caculator.test.js
+++ __tests__/caculator.test.js
@@ -1,63 +1,122 @@
 import App from '../src/js/App.js';
 
-const app = new App();
-
 describe('덧셈 테스트', () => {
+  const app = new App();
   app.operation = '+';
 
   test('1 + 1은 2이다.', () => {
-    app.log = [1, 1];
+    app.inputLog = [1, 1];
     app.calculate();
-    expect(app.total).toEqual(2);
+    expect(app.result).toEqual(2);
   });
+
   test('141 + 451은 592이다.', () => {
-    app.log = [141, 451];
+    app.inputLog = [141, 451];
     app.calculate();
-    expect(app.total).toEqual(592);
+    expect(app.result).toEqual(592);
   });
 });
 
 describe('뺄셈 테스트', () => {
+  const app = new App();
   app.operation = '-';
 
   test('2 - 1은 1이다.', () => {
-    app.log = [2, 1];
+    app.inputLog = [2, 1];
     app.calculate();
-    expect(app.total).toEqual(1);
+    expect(app.result).toEqual(1);
   });
+
   test('400 - 134는 266이다.', () => {
-    app.log = [400, 134];
+    app.inputLog = [400, 134];
     app.calculate();
-    expect(app.total).toEqual(266);
+    expect(app.result).toEqual(266);
   });
 });
 
 describe('곱셈 테스트', () => {
+  const app = new App();
   app.operation = 'X';
 
   test('2 * 2는 4이다.', () => {
-    app.log = [2, 2];
+    app.inputLog = [2, 2];
     app.calculate();
-    expect(app.total).toEqual(4);
+    expect(app.result).toEqual(4);
   });
+
   test('20 * 20은 400이다.', () => {
-    app.log = [20, 20];
+    app.inputLog = [20, 20];
     app.calculate();
-    expect(app.total).toEqual(400);
+    expect(app.result).toEqual(400);
   });
 });
 
 describe('나눗셈 테스트', () => {
+  const app = new App();
   app.operation = '/';
 
   test('4 / 2는 2이다.', () => {
-    app.log = [4, 2];
+    app.inputLog = [4, 2];
     app.calculate();
-    expect(app.total).toEqual(2);
+    expect(app.result).toEqual(2);
   });
   test('10 / 3은 3이다.', () => {
-    app.log = [10, 3];
+    app.inputLog = [10, 3];
     app.calculate();
-    expect(app.total).toEqual(3);
+    expect(app.result).toEqual(3);
+  });
+});
+
+describe('AC 테스트', () => {
+  const app = new App();
+
+  test('결과가 존재하지 않는 경우', () => {
+    app.calculate('clear');
+    expect(app.result).toEqual(null);
+    expect(app.inputLog.length).toEqual(0);
+  });
+
+  test('결과가 존재하는 경우', () => {
+    app.operation = '';
+    app.inputLog = [20];
+    app.result = 20;
+    app.resetCalculator();
+    app.clearCaculator();
+    expect(app.result).toEqual(null);
+    expect(app.inputLog.length).toEqual(0);
+  });
+});
+
+describe('최대 자릿수 테스트', () => {
+  const app = new App();
+
+  test('한자릿수 입력', () => {
+    app.addCurrentValue('1');
+    expect(app.currentValue.length <= 3).toEqual(true);
+  });
+
+  test('두자릿수 입력', () => {
+    app.addCurrentValue('1');
+    expect(app.currentValue.length <= 3).toEqual(true);
+  });
+
+  test('세자릿수 입력', () => {
+    app.addCurrentValue('1');
+    expect(app.currentValue.length <= 3).toEqual(true);
+  });
+
+  test('네자릿수 입력', () => {
+    app.addCurrentValue('1');
+    expect(app.currentValue.length <= 3).toEqual(true);
+  });
+});
+
+describe('소숫점 테스트', () => {
+  const app = new App();
+
+  test('4 / 3는 1이다.', () => {
+    app.inputLog = [4, 3];
+    app.calculate();
+    expect(app.result).toEqual(1);
   });
 });
