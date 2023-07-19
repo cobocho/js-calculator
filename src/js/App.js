e6280e5b8473884436d9241d7b85d09da0abac5c
cobocho <rlaalsrb1111@naver.com>
2023-07-19 14:38:22 +0900

    Feat: 초기화 및 연산자 이벤트 구현

--- src/js/App.js
+++ src/js/App.js
@@ -5,27 +5,60 @@ class App {
 
   operation = null;
 
-  total = 0;
+  currentValue = 0;
+
+  result = null;
 
   numberLog = [];
 
   init() {
     this.#screen = document.querySelector('#total');
     this.#setKeypad();
+    this.#setOperations();
+    this.#setModifier();
   }
 
   #setKeypad() {
     const digits = document.querySelector('.digits');
     digits.addEventListener('click', (e) => {
       const { value } = e.target.dataset;
-      this.total += value;
+      this.currentValue += value;
       this.#inputField += value;
+      this.#changeInputField();
+    });
+  }
+
+  #setModifier() {
+    const modifier = document.querySelector('.modifier');
+    modifier.addEventListener('click', () => {
+      this.#resetCalculator();
+    });
+  }
+
+  #setOperations() {
+    const operations = document.querySelector('.operations');
+    operations.addEventListener('click', (e) => {
+      if (this.operation) return;
+      const operation = e.target.dataset.value;
+      this.numberLog.push(Number(this.currentValue));
+      this.#inputField += operation;
+      this.operation = operation;
+      this.currentValue = 0;
       this.#changeInputField();
     });
   }
 
   #changeInputField() {
     this.#screen.textContent = this.#inputField;
+    console.log(this.numberLog);
+  }
+
+  #resetCalculator() {
+    if (this.result) this.#inputField = this.result;
+    else this.#inputField = '0';
+    this.#changeInputField();
+    if (!this.result) this.#inputField = '';
+    this.total = 0;
   }
 }
 
