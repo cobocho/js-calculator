272f2b266d9878d8113f2f6bfb0f39e38ca76ad4
cobocho <rlaalsrb1111@naver.com>
2023-07-19 13:09:50 +0900

    Chore: `eslint` 및 `prettier` 설정 재구성

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
-  extends: ['airbnb-base', 'prettier'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2022,
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/extensions': ['off'],
+    'class-methods-use-this': 'off',
+  },
 };
